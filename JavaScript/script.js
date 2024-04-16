af8bea03119d95071f33d9967f7d7052a7d71916
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-04-16 13:49:00 +0300

    added the function that selects an image randomly from the given array of paths

---
+++ JavaScript/script.js
@@ -0,0 +1,43 @@
+console.log("Hello World!");
+var Images = [
+  "Images/Advanced/Screenshot(63).png",
+  "Images/Advanced/Screenshot(64).png",
+  "Images/Advanced/Screenshot(65).png",
+  "Images/Advanced/Screenshot(66).png",
+  "Images/Advanced/Screenshot(67).png",
+  "Images/Advanced/Screenshot(68).png",
+  "Images/Advanced/Screenshot(69).png",
+  "Images/Advanced/Screenshot(70).png",
+];
+var Leetcode = [
+  'Images/Leetcode/leetcode_profile.png',
+  'Images/Leetcode/Screenshot (95).png',
+  'Images/Leetcode/Screenshot (97).png',
+  'Images/Leetcode/Screenshot (103).png',
+  'Images/Leetcode/Screenshot (104).png'
+
+ 
+  
+
+]
+function getRandomInt(n) {
+  return Math.floor(Math.random() * n);
+}
+function changeImage(Images, photoId) {
+  var imageElement = document.getElementById(photoId);
+  var currentImageSrc = imageElement.src;
+  currentImageSrc = currentImageSrc.substring(3, currentImageSrc.length);
+  var currentIndex = getRandomInt(Images.length); 
+  console.log('current index',currentIndex)
+  console.log('length',Images.length)
+  imageElement.src ='../' + Images[currentIndex];
+}
+
+
+  
+ 
+ 
+  
+ 
+  
+ 
